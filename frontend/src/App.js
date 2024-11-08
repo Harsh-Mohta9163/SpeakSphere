c88435a0cc65f1e65ea816261cff94c6864baa69
chandrahas121 <chandrahas7c@gmail.com>
2024-11-08 14:21:58 +0530

    edit1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Create from './components/Create';
 import Login from './components/LoginPage';
 import Register from './components/RegisterPage';
+import LanguageLearningHeader from './components/LanguageLearningHeader';
+import LanguageCourses from './components/LanguageCourses';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path = "" element={<Home/>}/>
+        <Route path = "" element={<LanguageLearningHeader/>}/>
         <Route path = "/login" element={<Login/>}/>
         <Route path = "/register" element={<Register/>}/>
+        <Route path = "/courses" element={<LanguageCourses/>}/>
+
       </Routes>
     </div>
   );
